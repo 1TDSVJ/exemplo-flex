946698471f785a912ccc2d9c158540273a147715
heitorborbamarini <rm92976@fiap.com.br>
2022-05-19 10:13:45 -0300

    add js e mais css

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//TUDO EM JS É OBJETO
+
+
+//STRING simples
+
+//let nome = "Heitor"
+
+//console.log(nome[3])
+
+//let numero = new Number(100)
+
+//Declarando um objeto
+
+var carro = {
+    modelo:"Uno",
+    fabricacao: 1997,
+    cor:"preto",
+    ligar:function(){
+        alert('LIGADO')
+    },
+    desligar:function(){
+        alert('DESLIGADO')
+    }
+    
+
+}
