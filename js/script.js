0b8890bdf1f778f69e9093ab2568eb4a5eacf9f2
DaniHenriqueAOM <rm94615@fiap.com.br>
2022-05-19 09:45:06 -0300

    ADD todas as mudanças feitas na aula.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// TUDO EM JAVASCRIPT É OBJETO
+
+// String simples
+// let nome = ""
+
+// let nome = new String("teste")
+
+// let numero = new Number(100)
+
+let nome = "Pedro"
+
+console.log(nome[3])
+
+// DECLARANDO UM OBJETO
+
+var carro = {
+    modelo: "Uno",
+    fabricacao: 1997,
+    cor: "Prata"
+    ligar:function(){
+        alert("LIGADO")
+    }
+    desligar:function(){
+        alert("DESLIGADO")
+    }
+}
