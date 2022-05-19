5a1b0d1aa97646e5ea1436db5e4ced0a8f2b7fd9
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-05-19 09:46:32 -0300

    Javascript and Flexbox

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+//TUDO EM JAVASCRIPT É OBJETO
+
+
+//String simples
+// let nome = "" ''
+
+// let numero = 100
+
+//DECLARANDO UM OBJETO
+var carro = {
+    modelo:"Uno",
+    fabricacao:1997,
+    cor:"prata",
+    ligar:function(){
+        alert('LIGADO')
+    },
+    desligar:function(){
+        alert('DESLIGADO')
+    }
+}
+
+
